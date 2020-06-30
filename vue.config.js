080fb95f73a86048d785983bc9ea3e9f42b54fc1
famall <user06>
2020-06-30 11:22:11 +0800

    vue.config formatter

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,20 @@
-
 // vue.config.js
-const path = require('path');
-const resolve = (dir) => path.join(__dirname, dir);
+const path = require('path')
+const resolve = dir => path.join(__dirname, dir)
 module.exports = {
-
   chainWebpack: config => {
-
     config.resolve.alias // 添加别名
-      .set("@", resolve("src"))
-      .set("@assets", resolve("src/assets"))
-      .set("@common", resolve("src/components/common"))
-      .set("@components", resolve("src/components"))
-      .set("@views", resolve("src/views"))
-      .set("@router", resolve("src/router"))
-      .set("@store", resolve("src/store"))
+      .set('@', resolve('src'))
+      .set('@assets', resolve('src/assets'))
+      .set('@common', resolve('src/components/common'))
+      .set('@components', resolve('src/components'))
+      .set('@views', resolve('src/views'))
+      .set('@router', resolve('src/router'))
+      .set('@store', resolve('src/store'))
   },
   devServer: {
-    overlay: { // 让浏览器 overlay 同时显示警告和错误
+    overlay: {
+      // 让浏览器 overlay 同时显示警告和错误
       warnings: true,
       errors: true
     },
@@ -24,9 +22,8 @@ module.exports = {
     port: 3000, // 端口号
     https: false, // https:{type:Boolean}
     open: true, //配置自动启动浏览器
-    hotOnly: true, // 热更新
+    hotOnly: true // 热更新
 
     // proxy: 'http://localhost:8080'   // 配置跨域处理,只有一个代理
   }
 }
-
